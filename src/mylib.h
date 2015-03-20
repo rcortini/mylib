5d792a6f7c49005d6c217dc8a5589008e2d0b1b7
Ruggero Cortini <cortini@lptl.jussieu.fr>
2015-03-20 14:50:37 +0100

    mylib.h completed

--- src/mylib.h
+++ src/mylib.h
@@ -7,5 +7,11 @@
 #include "average_variance.h"
 #include "histogram.h"
 #include "fit.h"
+#include "f_deriv.h"
+#include "f_interp.h"
+#include "f_root.h"
+#include "f_min.h"
+#include "f_multimin.h"
+#include "fdf_multimin.h"
 
 #endif
