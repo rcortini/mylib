ab7ef9eea33ef74ed0f34cc4a73028ab79abf1e3
Ruggero Cortini <cortini@lptl.jussieu.fr>
2015-03-20 16:21:59 +0100

    removed mystat from lib; fixed distribution errors

--- src/mylib.h
+++ src/mylib.h
@@ -2,16 +2,16 @@
 #define __MYLIB_H__
 
 /* include everything */
-#include "mylib_retcodes.h"
-#include "utils.h"
-#include "average_variance.h"
-#include "histogram.h"
-#include "fit.h"
-#include "f_deriv.h"
-#include "f_interp.h"
-#include "f_root.h"
-#include "f_min.h"
-#include "f_multimin.h"
-#include "fdf_multimin.h"
+#include <mylib/mylib_retcodes.h>
+#include <mylib/utils.h>
+#include <mylib/average_variance.h>
+#include <mylib/histogram.h>
+#include <mylib/fit.h>
+#include <mylib/f_deriv.h>
+#include <mylib/f_interp.h>
+#include <mylib/f_root.h>
+#include <mylib/f_min.h>
+#include <mylib/f_multimin.h>
+#include <mylib/fdf_multimin.h>
 
 #endif
